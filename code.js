fbe9415b26fe3acec18a6f3ff87d9ddcb0d9251f
zenghuihuang <huihui127127@gmail.com>
2021-09-29 20:52:40 +0100

    update

--- code.js
+++ code.js
@@ -1,18 +1,68 @@
-function Calculate() {
-    alert ("Hello World!")
+function Calculate(a,b,c) {
+    if(c=="+"){
+      
+      return a + b;
+     }
+     if(c=="-"){
+      return a - b;
+      
+    }
+    if(c=="*"){
+      
+      return a * b;
+    }
+    if(c=="/"){
+      
+      return a / b;
+    }
+
    }
 
 
    
-var first="";
+let first="";
+let operator="";
+let second="";
 function Display(a) {
 
   
-  if (a!=='+' || a!=='-' || a!=='/' || a!=='*' ||a!=='=' ||a!=='.'){
-    alert(typeof a)
-    first.concat(a.toString());
-    
-    document.getElementById("result").value=first;
+  
+  if (a!=='+' && a!=='-' && a!=='/' && a!=='*'){
+
+    if (operator==""){
+      first+=a;
+      document.getElementById("result").value=first;
+    }else{
+
+      second+=a;
+      document.getElementById("result").value=second;
+   }
     
   }
+  else{
+
+  operator=a;
+     
+   }
+
+   second+=a;
+   if(document.getElementById("result").value == undefined){
+     
+    document.getElementById("result").value=Calculate(first,second,operator);
+      
+        
+      }
+
+ 
+
+ 
 }
+
+function clearResult(){
+
+  document.getElementById("result").value="";
+  first="";
+  second="";
+  operator="";
+     
+   }
