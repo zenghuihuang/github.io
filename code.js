747a62f321cf8307e6d6002d19f4805d040d1b0a
zenghuihuang <huihui127127@gmail.com>
2021-09-29 19:35:17 +0100

    update

--- code.js
+++ code.js
@@ -1,3 +1,14 @@
 function Calculate() {
     alert ("Hello World!")
    }
+
+function Display(a) {
+
+  var first=""
+  if (a!=='+' || a!=='-' || a!=='/' || a!=='*' ||a!=='=' ||a!=='.'){
+    first=first+a;
+
+    document.getElementById("result").value=first;
+    
+  }
+}
